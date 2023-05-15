ef093dbef8629e4b6c785be70404c72dd7ba85e2
Кирьянов Артем Евгеньевич <aevkiryanov@sberbank.ru>
2023-05-15 11:01:15 +0300

    fix code

--- app.py
+++ app.py
@@ -1,97 +1,50 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Загрузка и редактирование изображений</title>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/fabric.js/4.5.0/fabric.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@neptunian/react-photo-editor"></script>
-    <script src="https://unpkg.com/resizerjs"></script>
-</head>
-<body>
-    <h1>Загрузка и редактирование изображений</h1>
-    <form method="POST" action="/upload" enctype="multipart/form-data">
-        <input type="file" name="file" accept="image/*">
-        <input type="submit" value="Загрузить">
-    </form>
+from flask import Flask, request, render_template, redirect, url_for
+import os
+
+app = Flask(__name__)
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+
+# Устанавливаем папку для загрузки файлов
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    # Проверяем разрешенное расширение файла
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/')
+def index():
+    # Отображаем форму загрузки файла
+    return render_template('index.html')
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    # Проверяем, что файл был отправлен в запросе
+    if 'file' not in request.files:
+        return redirect(request.url)
     
-    {% if file_url %}
-        <h2>Ссылка на изображение:</h2>
-        <a href="{{ file_url }}" target="_blank">{{ file_url }}</a>
+    file = request.files['file']
+    
+    # Проверяем, что файл имеет допустимое расширение
+    if file and allowed_file(file.filename):
+        # Сохраняем файл в папку uploads
+        filename = file.filename
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
         
-        <h2>Редактировать изображение:</h2>
-        <div id="canvas-container">
-            <canvas id="canvas" width="400" height="300"></canvas>
-        </div>
-        <br>
-        <button onclick="openEditor()">Изменить изображение</button>
-    {% endif %}
+        # Создаем ссылку на загруженный файл
+        file_url = url_for('uploaded_file', filename=filename, _external=True)
+        
+        # Возвращаем главную страницу с ссылкой на загруженное изображение
+        return render_template('index.html', file_url=file_url)
     
-    <script>
-        var canvas = new fabric.Canvas('canvas');
-        var imageObject = null;
-        var editorInstance = null;
+    # Если файл имеет недопустимое расширение, перенаправляем пользователя обратно на форму загрузки
+    return redirect(request.url)
 
-        function loadImage() {
-            var fileInput = document.querySelector('input[type="file"]');
-            var file = fileInput.files[0];
-            var reader = new FileReader();
-            reader.onload = function(event) {
-                var imageUrl = event.target.result;
-                fabric.Image.fromURL(imageUrl, function(img) {
-                    canvas.clear();
-                    canvas.add(img);
-                    imageObject = img;
-                });
-            };
-            reader.readAsDataURL(file);
-        }
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    # Отображаем загруженный файл
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
-        function openEditor() {
-            var fileInput = document.querySelector('input[type="file"]');
-            if (fileInput.files.length === 0) {
-                alert('Сначала загрузите изображение.');
-                return;
-            }
-
-            var canvasContainer = document.getElementById('canvas-container');
-            canvasContainer.style.display = 'none';
-
-            editorInstance = new PhotoEditor(canvas, imageObject);
-            editorInstance.open(function(updatedImage) {
-                canvasContainer.style.display = 'block';
-                canvas.clear();
-                canvas.add(updatedImage);
-                imageObject = updatedImage;
-            });
-        }
-
-        function saveImage() {
-            if (editorInstance) {
-                var resizedImage = editorInstance.getImageScaledToCanvas();
-                var file = ResizerJS.dataURLtoFile(resizedImage.toDataURL(), 'resized_image.png');
-
-                var formData = new FormData();
-                formData.append('file', file);
-
-                fetch('/upload', {
-                    method: 'POST',
-                    body: formData
-                })
-                .then(response => response.json())
-                .then(data => {
-                    var fileUrl = data.file_url;
-                    var linkElement = document.createElement('a');
-                    linkElement.href = fileUrl;
-                    linkElement.textContent = 'Ссылка на измененное изображение';
-                    linkElement.target = '_blank';
-
-                    var containerElement = document.getElementById('canvas-container');
-                    containerElement.appendChild(linkElement);
-                })
-                .catch(error => {
-                    console.error('Ошибка при сохранении изображения:', error);
-                });
-            }
-        }
-    </script>
-</body>
-</html>
+if __name__ == '__main__':
+    app.run()
